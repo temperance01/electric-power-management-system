5019e2a15fe39dffa34703617a33782bd9d148a0
temperance01 <rralphsarz@gmail.com>
2023-05-09 18:50:48 +0800

    commit

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-#main file of the program 
-import flet 
+import flet as ft
 from flet import *
 
-import interface
+def login_page(page: ft.Page):
+    page.title = "EPM System"
+    page.vertical_alignment = "center"
+    page.window_height = 600
+    page.window_width = 300
+    
+    t = ft.Text(value="Hello, world!", color="green")
+    page.controls.append(t)
 
-interface.login_page()
+    page.update()
+    
+
+ft.app(target=login_page)
 
